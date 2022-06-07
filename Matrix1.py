6fefd4d9073e323c0d37c78595bf2f56d1db4a5a
Dazovscky <dazovsckyao@gmail.com>
2022-06-07 11:20:53 +0300

    first commit

---
+++ Matrix1.py
@@ -0,0 +1,14 @@
+rows = int(input())
+cols = int(input())
+matrix = []
+list = []
+for r in range(rows):
+    for c in range(cols):
+        word = input()
+        list.append(word)
+    matrix.append(list)
+    list = []
+for r in range(rows):
+    for c in range(cols):
+        print(matrix[r][c], end=' ')
+    print()
